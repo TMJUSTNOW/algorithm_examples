e7064e2d3058fb24d1d34ca5410a334f508cf892
Theja Tulabandhula <theja@mit.edu>
2013-12-19 10:35:21 -0500

    a graph datastructure

--- graph.py
+++ graph.py
@@ -1,24 +1,38 @@
 class Graph:
 	"""
-	A simple graph Data structure.
+
+	A simple graph Data structure using the adjacency-list representation.
 	Assume vertices are numbered from 0 to nVertices-1
+
 	"""
 	nVertices = 0 #number of vertices
 	adjacencyList = [] #We will represent edges using adjacency-list representation.
 	#adjacencyList will be a list of lists.
 
 	def __init__(self,nVertices=0):
-		""" (Graph,int) -> NoneType
+		''' (Graph,int) -> NoneType
+
+		Initialize the graph with the given vertices.
+
 		>>> myGraph  = Graph(3)
 		>>> myGraph.nVertices
 		3
-		"""
+		'''
 		self.nVertices = nVertices
 		self.adjacencyList = [[] for x in range(self.nVertices)]
 
 
 	def addEdge(self,v,w):
-		"""Primary: Add an edge between vertices v and w"""
+		"""(Graph,int,int) -> NoneType
+
+		Add an edge between vertices v and w
+
+		>>> myGraph = Graph(2)
+		>>> myGraph.addEdge(0,1)
+		>>> print myGraph
+
+
+		"""
 		if v==w:
 			return #todo: self loops should raise exception
 		elif w in self.adj(v) or v in self.adj(w):
@@ -27,11 +41,24 @@ class Graph:
 		self.adjacencyList[w].append(v)
 
 	def adj(self,v):
-		"""Returns a list of vertices adjacent to vertex v"""
+		"""(Graph,int) -> list
+
+		Returns a list of vertices adjacent to vertex v
+
+		>>> myGraph = Graph(2)
+		>>> myGraph.addEdge(0,1)
+		>>> myGraph.adj(1)
+		[0]
+
+		"""
 		return self.adjacencyList[v]
 
 	def __str__(self):
-		"""String representation"""
+		"""(Graph) -> String
+		
+		String representation
+
+		"""
 		line1 = 'Graph has {0} vertices.\n'.format(self.nVertices)
 		line2 = ''
 		nEdges = 0
@@ -43,22 +70,21 @@ class Graph:
 		return line1+line3+line2[:-1]#removing the last '\n' for prettyness.
 
 	def addEdgesFromAdjacencyList(self,adjacencyList=[]):
-		"""
-		Append an adjacency list of edges to the graph. This method is not essential.
+		"""(Graph,list of lists) -> NoneType
+
+		Append an adjacency list of edges to the graph. 
+		This method is not essential.
+		
 		"""
 		for v,nbhd in enumerate(adjacencyList):
 			for w in nbhd:
 				addEdge(v,w)
 
+#Decoupleing graph data type from graph processing
+def dfs():
+	print "Not implemented yet."
 
-def main():
-	print "Graph Processing Data Structure Main function. Here are some examples."
-
-	myGraph = Graph(4)
-	myGraph.addEdge(0,2)
-	myGraph.addEdge(1,2)
-	myGraph.addEdge(3,2)
-	print myGraph
 
 if __name__=="__main__":
-	main()
+	dfs()
+
