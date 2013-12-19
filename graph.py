f322f75b5c826c5f61fd2fe3c94350b16e6d618d
Theja Tulabandhula <theja@mit.edu>
2013-12-19 09:44:46 -0500

    a graph datastructure

--- graph.py
+++ graph.py
@@ -1,28 +1,32 @@
-class Graph():
+class Graph:
 	"""
 	A simple graph Data structure.
-
-
-
 	"""
-	V = 0
-	E = 0
-	def a():
-		self.V = 0
-		self.E = 0
+	V = 0 #number of vertices
+	E = 0 #number of edges
+	def __init__(self,nVertices=0):
+		""" (Graph,int) -> NoneType
+		>>> myGraph  = Graph(3)
+		>>> myGraph.V
+		3
+		"""
+		self.V = nVertices
 
-	def addEdge(v,w):
+	def addEdge(self,v,w):
 		"""Add an edge between vertices v and w"""
 
-	def adj(v):
+	def adj(self,v):
 		"""Vertices adjacent to vertex v"""
 
-	def __str__():
-		"""String representation""
+	def __str__(self):
+		"""String representation"""
 
 
 def main():
 	print "Graph Processing Data Structure Main function. Here are some examples."
+
+	myGraph = Graph(10)
+	print myGraph
 
 if __name__=="__main__":
 	main()
