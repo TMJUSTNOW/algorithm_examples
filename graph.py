c735fb37ea5b079cb182498bb365c4c4f4c6dc79
Theja Tulabandhula <theja@mit.edu>
2013-12-19 09:34:14 -0500

    initial commit: trying out a graph datastructure

---
+++ graph.py
@@ -0,0 +1,28 @@
+class Graph():
+	"""
+	A simple graph Data structure.
+
+
+
+	"""
+	V = 0
+	E = 0
+	def a():
+		self.V = 0
+		self.E = 0
+
+	def addEdge(v,w):
+		"""Add an edge between vertices v and w"""
+
+	def adj(v):
+		"""Vertices adjacent to vertex v"""
+
+	def __str__():
+		"""String representation""
+
+
+def main():
+	print "Graph Processing Data Structure Main function. Here are some examples."
+
+if __name__=="__main__":
+	main()
---
+++ readme.md
@@ -0,0 +1,4 @@
+A list of example algorithms and data structures. These can be found in standard algorithm textbooks.
+
+  - Graph (if we don't want to use the amazing networkx module)
+
