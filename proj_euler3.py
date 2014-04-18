e8bd4c4228f7374be235b369bc5c2522fdb9f746
Theja Tulabandhula <theja2t@gmail.com>
2014-04-18 06:01:51 -0400

    prob 23 start

--- proj_euler3.py
+++ proj_euler3.py
@@ -96,9 +96,26 @@ def p22():
     total += (e+1)*sum([alphadict[y] for y in x])
   print total
 
+def p23():
+  '''
+  A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+  A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+
+  As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+  Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+  '''
+  abundant_nr = []
+  for i in range(1,28123):
+    if sum_prop_div(i) > i:
+      abundant_nr.append(i)
+  print abundant_nr
+
 def main():
   # p21()
-  p22()
+  # p22()
+  p23()
   # pdb.set_trace()
   
 if __name__=="__main__":
