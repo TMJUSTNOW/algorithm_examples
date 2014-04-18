ccf64f06b46ea1fce9e7656e110a8ee25243d568
Theja Tulabandhula <theja2t@gmail.com>
2014-04-17 23:38:50 -0400

    prob 22

--- proj_euler3.py
+++ proj_euler3.py
@@ -77,8 +77,24 @@ def p21():
   print sum(list(set(amicable))) #31626
 
 def p22():
-  print "hi"
+  '''
+  Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
 
+  For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 * 53 = 49714.
+
+  What is the total of all the name scores in the file?
+  '''
+  abc='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  alphadict = {x:e+1 for e,x in enumerate(abc)}
+  print alphadict 
+  with open('names.txt','rb') as fin:
+    names = fin.read().split(',')
+    names = sort([x.strip("\"") for x in names])
+  print names
+  total = 0
+  for e,x in enumerate(names):
+    total += (e+1)*sum([alphadict[y] for y in x])
+  print total
 
 def main():
   # p21()
